f26c54cba94e8e0a575bd75e03113345f60b1eee
SoumilMalik24 <soumil4malik@gmail.com>
2025-12-22 07:43:09 +0530

    meta data init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smart_support",
+    version="0.0.1",
+    author="Soumil Malik",
+    author_email="soumil4malik@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
